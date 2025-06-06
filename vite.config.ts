97c950b23375a42f2e9dad2de73f55ea147d3469
soeheinsh <soetheindev@gmail.com>
2025-06-06 14:55:32 +0630

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -17,5 +17,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: process.env.VITE_BASE_PATH || "/",
+  base: process.env.VITE_BASE_PATH || "/react-vite-deploy",
 }));
